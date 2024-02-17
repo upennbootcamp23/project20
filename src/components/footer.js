2491a0de1852cbb5e167d53e66f124ad42d8e3fc
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-17 16:32:14 -0500

    updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ function Footer() {
                 <a href="https://github.com/upennbootcamp2323" target="_blank"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" className="icon"/></a>
             </div>
             <p className='footer-info'>
-                &copy; 2024 Alexander Egerev  |  <a href="https://github.com/upennbootcamp23/project20/blob/main/src/assets/Egerev_Resume2023.docx.pdf" className="link">download resume</a>  
+                &copy; 2024 Alexander Egerev  |  <a href="https://github.com/upennbootcamp23/project20/blob/main/src/assets/Egerev_Resume2023.docx.pdf" className="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51"></a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
             </p>
 
         </div>
