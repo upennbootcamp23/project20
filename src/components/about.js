3e60fde8bb440026dbce73e47596daa2d3989de8
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-17 10:17:04 -0500

    added picture and about me

---
+++ src/components/about.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProfilePic from '../assets/images/avatar.png';
+import BerkeleyLogo from '../assets/images/berkeley.png';
+import PittsburghLogo from '../assets/images/pittsburgh.png';
+import CarletonLogo from '../assets/images/carleton.png';
+import '../App.css';
+
