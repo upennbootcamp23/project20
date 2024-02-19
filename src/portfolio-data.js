1f9995166e6bd39fc6caf9c4426904fd69dbc19c
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-19 11:21:23 -0500

    updated portoflio

---
+++ src/portfolio-data.js
@@ -0,0 +1,94 @@
+import HoriSEOn from './assets/images/horizon-seo.jpg';
+import Portfolio from './assets/images/portfolio.PNG';
+import PasswordGen from './assets/images/password.png';
+import JavaScriptQuiz from './assets/images/js-quiz.png';
+import JSCalendar from './assets/images/js-calendar.png';
+import Weather from './assets/images/weather.png';
+import Leaflit from './assets/images/leaflit.PNG'
+import NoteTaker from './assets/images/mister-rogers-note.png';
+import CMSBlog from './assets/images/one-piece-blog.png';
+import MyGarden from './assets/images/garden.png'
+import JustATextEditor from './assets/images/jate.png';
+import JustATextEditor from './assets/images/jate.png';
+
+export default [
+    {
+        "id": 1,
+        "name": "HoriSEOn",
+        "image": HoriSEOn,
+        "github": "https://github.com/upennbootcamp23/seo-website-refactor",
+        "deploy": "https://upennbootcamp23.github.io/seo-website-refactor/"
+    },
+    {
+        "id": 2,
+        "name": "My Portfolio",
+        "image": Portfolio,
+        "github": "https://github.com/upennbootcamp23/egerev_portfolio-website",
+        "deploy": "https://upennbootcamp23.github.io/egerev_portfolio-website/"
+    },
+    {
+        "id": 3,
+        "name": "Password Generator",
+        "image": PasswordGen,
+        "github": "https://github.com/upennbootcamp23/egerev_password-website-refactor",
+        "deploy": "https://upennbootcamp23.github.io/egerev_password-website-refactor/"
+    },
+    {
+        "id": 4,
+        "name": "JavaScript Quiz",
+        "image": JavaScriptQuiz,
+        "github": "https://github.com/upennbootcamp23/egerev_javascript-quiz-website",
+        "deploy": "https://upennbootcamp23.github.io/egerev_javascript-quiz-website/"
+    },
+    {
+        "id": 5,
+        "name": "Workday Planner",
+        "image": JSCalendar,
+        "github": "https://github.com/upennbootcamp23/egerev_planner-website-refactor",
+        "deploy": "https://upennbootcamp23.github.io/egerev_planner-website-refactor/"
+    },
+    {
+        "id": 6,
+        "name": "Weather Dashboard",
+        "image": Weather,
+        "github": "https://github.com/upennbootcamp23/egerev_weather-website",
+        "deploy": "https://upennbootcamp23.github.io/egerev_weather-website/"
+    },
+    {
+        "id": 7,
+        "name": "LeafLit",
+        "image": Leaflit,
+        "github": "https://github.com/Aaron-Heath/leaflit",
+        "deploy": "https://aaron-heath.github.io/leaflit/"
+    },
+    {
+        "id": 8,
+        "name": "Express Note Taker",
+        "image": NoteTaker,
+        "github": "https://github.com/upennbootcamp23/egerev_note-taker-refactored",
+        "deploy": "https://egerev-note-taker-refactored-e24096d10342.herokuapp.com/"
+    },
+    {
+        "id": 9,
+        "name": "CMS Blog Site",
+        "image": CMSBlog,
+        "github": "https://github.com/upennbootcamp23/egerev_CMS-blog-site",
+        "deploy": "https://egerev-cmsblogsite-1c7e5ee8496d.herokuapp.com/"
+    },
+    {
+        "id": 10,
+        "name": "Garden Planner",
+        "image": MyGarden,
+        "github": "https://github.com/jeffch19/garden-bliss",
+        "deploy": "https://garden-planner19-05823e4485e2.herokuapp.com/"
+    },
+    
+    {
+        "id": 11,
+        "name": "Just Another Text Editor",
+        "image": JustATextEditor,
+        "github": "https://github.com/upennbootcamp23/egerev_jate-refactor",
+        "deploy": "https://egerev-just-another-text-editor.onrender.com/"
+    }
+
+]
